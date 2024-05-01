0a97fb512a4f0a8e9558d103615cbbf80c2adde3
Zakaria Sharief <zakriyarahman@gmail.com>
2024-04-30 22:45:35 -0500

    Initial Commit

---
+++ src/lib.rs
@@ -0,0 +1,89 @@
+use wasm_bindgen::prelude::*;
+use wee_alloc::WeeAlloc;
+
+#[global_allocator]
+static ALLOC: WeeAlloc = WeeAlloc::INIT;
+
+#[wasm_bindgen]
+#[derive(PartialEq)]
+pub enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
+}
+
+struct SnakeCell(usize);
+
+struct Snake {
+    body: Vec<SnakeCell>,
+    direction: Direction,
+}
+
+impl Snake {
+    fn new(spawn_index: usize) -> Snake {
+        Snake {
+            body: vec!(SnakeCell(spawn_index)),
+            direction: Direction::Left,
+        }
+    }
+}
+
+#[wasm_bindgen]
+pub struct World {
+    width: usize,
+    size: usize,
+    snake: Snake,
+}
+
+#[wasm_bindgen]
+impl World {
+    pub fn new(width: usize, snake_idx: usize) -> World {
+        World {
+            width,
+            size: width * width,
+            snake: Snake::new(snake_idx)
+        }
+    }
+
+    pub fn width(&self) -> usize {
+        self.width
+    }
+
+    pub fn snake_head_idx(&self) -> usize {
+        self.snake.body[0].0
+    }
+
+    pub fn change_snake_dir(&mut self, direction: Direction) {
+        self.snake.direction = direction;
+
+    }
+
+    pub fn update(&mut self) {
+        let snake_idx = self.snake_head_idx();
+        let (row, col) = self.index_to_cell(snake_idx);
+        let (row, col) = match self.snake.direction {
+            Direction::Right => (row, (col + 1) % self.width()),
+            Direction::Left => (row, (col - 1) % self.width()),
+            Direction::Up => ((row - 1) % self.width(), col),
+            Direction::Down => ((row + 1) % self.width(), col),
+        };
+
+        let next_idx = self.cell_to_index(row, col);
+        self.set_snake_head(next_idx);
+    }
+
+    fn set_snake_head(&mut self, idx: usize) {
+        self.snake.body[0].0 = idx;
+    }
+
+    fn index_to_cell (&self, idx: usize) -> (usize, usize) {
+        (idx / self.width, idx % self.width)
+    }
+
+    fn cell_to_index (&self, row: usize, col: usize) -> usize {
+        (row * self.width) + col
+    }
+}
+
+// wasm-pack build --target web
---
+++ www/bootstrap.js
@@ -0,0 +1,5 @@
+
+
+
+import("./index")
+  .catch(e => console.error("Error importing index.js :", e))
