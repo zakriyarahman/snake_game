4a8cb9a8d3b4b2752e1952f02902efc10033b449
Zakaria Sharief <zakriyarahman@gmail.com>
2024-05-02 21:00:16 -0500

    Formatting

--- www/index.ts
+++ www/index.ts
@@ -1,94 +1,83 @@
 import init, { World, Direction } from "snake_game";
 
 init().then(wasm => {
-  const CELL_SIZE = 20;
-  const WORLD_WIDTH = 8;
-  const snakeSpawnIdx = Date.now() % (WORLD_WIDTH * WORLD_WIDTH);
+    const CELL_SIZE = 20;
+    const WORLD_WIDTH = 8;
+    const snakeSpawnIdx = Date.now() % (WORLD_WIDTH * WORLD_WIDTH);
 
-  const world = World.new(WORLD_WIDTH, snakeSpawnIdx);
-  const worldWidth = world.width();
+    const world = World.new(WORLD_WIDTH, snakeSpawnIdx);
+    const worldWidth = world.width();
 
-  const canvas = <HTMLCanvasElement> document.getElementById("snake-canvas");
-  const ctx = canvas.getContext("2d");
+    const canvas = <HTMLCanvasElement> document.getElementById("snake-canvas");
+    const ctx = canvas.getContext("2d");
 
-  canvas.height = worldWidth * CELL_SIZE;
-  canvas.width = worldWidth * CELL_SIZE;
+    canvas.height = worldWidth * CELL_SIZE;
+    canvas.width = worldWidth * CELL_SIZE;
 
-  const snakeCellPtr = world.snake_cells();
-  const snakeLen = world.snake_length();
-  
-  const snakeCells = new Uint32Array(
-    wasm.memory.buffer,
-    snakeCellPtr,
-    snakeLen
-  );
+    document.addEventListener("keydown", e => {
+        switch(e.code) {
+            case "ArrowUp":
+            world.change_snake_dir(Direction.Up);
+            break;
+            case "ArrowRight":
+            world.change_snake_dir(Direction.Right);
+            break;
+            case "ArrowDown":
+            world.change_snake_dir(Direction.Down);
+            break;
+            case "ArrowLeft":
+            world.change_snake_dir(Direction.Left);
+            break;
+        }
+    })
 
-  console.log(snakeCells);
+    function drawWorld() {
+        ctx.beginPath();
 
-  document.addEventListener("keydown", e => {
-    switch(e.code) {
-      case "ArrowUp":
-        world.change_snake_dir(Direction.Up);
-        break;
-      case "ArrowRight":
-        world.change_snake_dir(Direction.Right);
-        break;
-      case "ArrowDown":
-        world.change_snake_dir(Direction.Down);
-        break;
-      case "ArrowLeft":
-        world.change_snake_dir(Direction.Left);
-        break;
-    }
-  })
+        for (let x = 0; x < worldWidth + 1; x++) {
+            ctx.moveTo(CELL_SIZE * x, 0);
+            ctx.lineTo(CELL_SIZE * x, worldWidth * CELL_SIZE)
+        }
 
-  function drawWorld() {
-    ctx.beginPath();
+        for (let y = 0; y < worldWidth + 1; y++) {
+            ctx.moveTo(0, CELL_SIZE * y);
+            ctx.lineTo(worldWidth * CELL_SIZE, CELL_SIZE * y)
+        }
 
-    for (let x = 0; x < worldWidth + 1; x++) {
-      ctx.moveTo(CELL_SIZE * x, 0);
-      ctx.lineTo(CELL_SIZE * x, worldWidth * CELL_SIZE)
+        ctx.stroke();
     }
 
-    for (let y = 0; y < worldWidth + 1; y++) {
-      ctx.moveTo(0, CELL_SIZE * y);
-      ctx.lineTo(worldWidth * CELL_SIZE, CELL_SIZE * y)
+    function drawSnake() {
+        const snakeIdx = world.snake_head_idx();
+        const col = snakeIdx % worldWidth;
+        const row = Math.floor(snakeIdx / worldWidth);
+
+        ctx.beginPath();
+        ctx.fillRect(
+            col * CELL_SIZE,
+            row * CELL_SIZE,
+            CELL_SIZE,
+            CELL_SIZE
+        );
+        ctx.stroke();
     }
 
-    ctx.stroke();
-  }
+    function paint() {
+        drawWorld();
+        drawSnake();
+    }
 
-  function drawSnake() {
-    const snakeIdx = world.snake_head_idx();
-    const col = snakeIdx % worldWidth;
-    const row = Math.floor(snakeIdx / worldWidth);
+    function update() {
+        const fps = 10;
+        setTimeout(() => {
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            world.update();
+            paint();
+            // the method takes a callback to invoked before the next repaint
+            requestAnimationFrame(update)
+        }, 1000 / fps)
+    }
 
-    ctx.beginPath();
-    ctx.fillRect(
-      col * CELL_SIZE,
-      row * CELL_SIZE,
-      CELL_SIZE,
-      CELL_SIZE
-    );
-    ctx.stroke();
-  }
-
-  function paint() {
-    drawWorld();
-    drawSnake();
-  }
-
-  function update() {
-    const fps = 10;
-    setTimeout(() => {
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
-      world.update();
-      paint();
-      // the method takes a callback to invoked before the next repaint
-      requestAnimationFrame(update)
-    }, 1000 / fps)
-  }
-
-  paint();
-  update();
+    paint();
+    update();
 })
